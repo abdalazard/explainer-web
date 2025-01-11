b4b5fa9c8fdbfaa1cafbf996f02e21754bf71b7a
abdalazard <abdalazard@gmail.com>
2025-01-11 01:07:18 -0300

    att

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/AnalyzeerExtension/', // Nome do seu repositório
+  base: '/AnalyzeerExtension/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
