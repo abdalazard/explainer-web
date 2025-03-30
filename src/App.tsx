31f48f0561223ccc0c36edec370998f08805c5c5
abdalazard <abdalazard@gmail.com>
2025-03-30 16:39:06 -0300

    corrige basename

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/explainer-web">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/registro" element={<Registro />} />
