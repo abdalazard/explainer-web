e8bbce37957e346536f4b2636ff94f8333ff4413
abdalazard <abdalazard@gmail.com>
2025-03-30 17:26:08 -0300

    att 2

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <HashRouter basename="/explainer-web">
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/registro" element={<Registro />} />
